794d78cb611ff30d3cd5e095c8d36cd80ee07471
ytendx <endx@web.de>
2021-12-13 13:40:51 +0100

    Added Range C

--- src/main/java/de/ytendx/xac/checks/combat/RangeCheck.java
+++ src/main/java/de/ytendx/xac/checks/combat/RangeCheck.java
@@ -3,6 +3,8 @@ package de.ytendx.xac.checks.combat;
 import de.ytendx.xac.XACMain;
 import de.ytendx.xac.notify.Notifyer;
 import de.ytendx.xac.notify.type.CheckType;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -34,6 +36,20 @@ public class RangeCheck implements Listener {
                 Notifyer.notify(((Player) event.getDamager()), CheckType.RANGE_B);
             }
         }
+
+        if(XZDif(event.getDamager().getLocation(), event.getEntity().getLocation()) > 6 && event.getDamager() instanceof Player
+                && !((Player) event.getDamager()).getGameMode().equals(GameMode.CREATIVE)){
+            if(!XACMain.getPlugin(XACMain.class).isSilent())event.setDamage(0);
+            Notifyer.notify(((Player) event.getDamager()), CheckType.RANGE_C);
+        }
+    }
+
+    private double difference(double f1, double f2){
+        return (f1 > f2 ? f1 - f2 : f2 - f1);
+    }
+
+    private double XZDif(Location loc1, Location loc2){
+        return difference(loc1.getX(), loc2.getX()) + difference(loc1.getZ(), loc2.getZ());
     }
 
 }
